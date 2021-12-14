12a57bc4bbebded903af650fb61865c650339670
Eugenie Cheng <eugenie.630.cheng@gmail.com>
2021-12-15 00:07:09 +0800

    Day2

--- day2/day2.go
+++ day2/day2.go
@@ -1,3 +1,6 @@
+/*
+--- Day 2: Dive! ---
+*/
 package day2
 
 import (
@@ -5,9 +8,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func Part1()  {
+	var temp int
+	horizontal := 0
+	depth := 0
 	file, err := os.Open("./input/d2.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -16,15 +23,91 @@ func Part1()  {
 	// optionally, resize scanner's capacity for lines over 64K, see next example
 	scanner.Split(bufio.ScanWords)
 
-	var input []string
+	//var input []string
 
 	for scanner.Scan() {
-		input = append(input, scanner.Text())
+		switch scanner.Text(){
+			case "forward":
+				scanner.Scan()
+				temp,err = strconv.Atoi(scanner.Text())
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				horizontal+= temp
+			case "up":
+				scanner.Scan()
+				temp,err = strconv.Atoi(scanner.Text())
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				depth -= temp
+			case "down":
+				scanner.Scan()
+				temp,err = strconv.Atoi(scanner.Text())
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				depth += temp
+		}
 	}
-	fmt.Println(input[1])
+
+	fmt.Println(horizontal * depth)
 }
+
+func Part2()  {
+	var aim int
+	var temp int
+	horizontal := 0
+	depth := 0
+	file, err := os.Open("./input/d2.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	scanner.Split(bufio.ScanWords)
+
+	//var input []string
+
+	for scanner.Scan() {
+		switch scanner.Text(){
+		case "forward":
+			scanner.Scan()
+			temp,err = strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(2)
+			}
+			horizontal+= temp
+			depth += aim*temp
+		case "up":
+			scanner.Scan()
+			temp,err = strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(2)
+			}
+			aim -= temp
+		case "down":
+			scanner.Scan()
+			temp,err = strconv.Atoi(scanner.Text())
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(2)
+			}
+			aim += temp
+		}
+	}
+
+	fmt.Println(horizontal * depth)
+}
+
 func Task(){
 	Part1()
+	Part2()
 }
 
 
